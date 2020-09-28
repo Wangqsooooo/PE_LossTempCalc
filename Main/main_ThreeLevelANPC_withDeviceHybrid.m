3539d6cdea661e05424c8fa06ca6bd2268363689
Wangqsooooo <3150102980@zju.edu.cn>
2020-09-28 19:55:53 +0800

    DeviceHybrid-0928

--- Main/main_ThreeLevelANPC_withDeviceHybrid.m
+++ Main/main_ThreeLevelANPC_withDeviceHybrid.m
@@ -6,12 +6,23 @@ Vdc = topology.Path(end, end-1)-topology.Path(1, end-1); % Vdc等于逆变器输出侧最
 cload = Load(Vdc, 4000, 0.02, 1e-3, 400, 100e3, 100, 1, 'PF', 0.9524); % load是matlab的关键词, 换成cload
 waves = Waves(cload, topology.Nums, 0.06);
 waves.ThreeLevel_ANPC_SingleCurrentPath(cload, 0, [1 2 3 4 5 6]);
-waves.Control(7:8, :) = waves.Control(2:3, :);
+delay = 5e-6;
+shift1 = zeros(size(waves.Control(2, :))); shift2 = shift1;
+shift1(1:end-1) = circshift(waves.Control(2, 1:end-1), round(delay/waves.Ts));
+shift1(end) = waves.Control(2, end-round(delay/waves.Ts));
+shift2(1:end-1) = circshift(waves.Control(2, 1:end-1), -round(delay/waves.Ts));
+shift2(end) = waves.Control(2, 1+round(delay/waves.Ts));
+waves.Control(7, :) = waves.Control(2, :)>0.5 & shift1>0.5 & shift2>0.5;
+shift1(1:end-1) = circshift(waves.Control(3, 1:end-1), round(delay/waves.Ts));
+shift1(end) = waves.Control(3, end-round(delay/waves.Ts));
+shift2(1:end-1) = circshift(waves.Control(3, 1:end-1), -round(delay/waves.Ts));
+shift2(end) = waves.Control(3, 1+round(delay/waves.Ts));
+waves.Control(8, :) = waves.Control(3, :)>0.5 & shift1>0.5 & shift2>0.5;
 waves.notify('ControlChanged'); % 由于是自己配置了一部分控制信号(7号和8号开关器件的信号)
                                 % 因此需要发出一个通知
 waves.ShortCircuit_Check(topology.HB_Restriction);
 waves.Output_Waves_Calc(topology.Path, cload);
-% h = figure(1);
+% h = figure(2);
 % h = waves.Output_Waves_Display(h);
 SiC_MOSFET = load('.\devices\Cree_C3M0015065K_SiC_650V.mat');
 Si_IGBT = load('.\devices\Infineon_IKZ75N65EL5_650V_withoutRecovery.mat');
--- src/@Waves/ThreeLevel_ANPC_SingleCurrentPath.m
+++ src/@Waves/ThreeLevel_ANPC_SingleCurrentPath.m
@@ -9,18 +9,23 @@ t = 0:obj.Ts:obj.Period;
 t_bias = round(t./obj.Ts);
 t_bias = floor(t_bias.*load.freq.*obj.Ts) ./ load.freq;
 modulation = load.ma .* sin(2.*pi./load.T.*t_bias-phaseshift);
+shift_modulation = zeros(size(modulation));
+shift_modulation(1:end-1) = circshift(modulation(1:end-1), round(1/load.freq/obj.Ts));
+shift_modulation(end) = modulation(end-round(1/load.freq/obj.Ts));
 carrier = zeros(2, length(t));
 carrier(1, :) = (rem(t.*load.freq, 1)<1/2) .* (1-2.*rem(t.*load.freq, 1)) ...
     + (rem(t.*load.freq, 1)>=1/2) .* (-1+2.*rem(t.*load.freq, 1));
 carrier(2, :) = carrier(1, :) - 1;
 control = zeros(6, length(t));
-control(1, :) = modulation >= 0;
-control(2, :) = (modulation >= 0 & modulation >= carrier(1, :)) ...
-                    | (modulation < 0 & modulation >= carrier(2, :));
+% 小数点后10位精度
+control(1, :) = (modulation >= 1e-10) ...
+    | (modulation<1e-10 & modulation>-1e-10 & shift_modulation<-1e-10);
+control(4, :) = ~control(1, :);
+control(5, :) = control(4, :);
+control(6, :) = control(1, :);
+control(2, :) = (control(1, :) & modulation >= carrier(1, :)) ...
+                    | (control(4, :) & modulation >= carrier(2, :));
 control(3, :) = ~control(2, :);
-control(4, :) = modulation < 0;
-control(5, :) = modulation < 0;
-control(6, :) = modulation >= 0;
 if strcmp(options.DisplayMode, 'No')
     obj.Control(order, :) = control;
     obj.ControlSet(order, :) = 1;
