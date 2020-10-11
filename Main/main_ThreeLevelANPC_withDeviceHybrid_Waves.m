60bb071a7dfa78596ccdc38c1e4faf243daee5b8
Wangqsooooo <3150102980@zju.edu.cn>
2020-10-11 22:09:29 +0800

    DeviceHybrid-1011

--- Main/main_ThreeLevelANPC_withDeviceHybrid_Waves.m
+++ Main/main_ThreeLevelANPC_withDeviceHybrid_Waves.m
@@ -7,7 +7,18 @@ Vdc = topology.Path(end, end-1)-topology.Path(1, end-1); % Vdc等于逆变器输出侧最
 cload = Load(Vdc, 4000, 0.02, 1e-3, 400, 100e3, 100, 1, 'PF', 0.9524); % load是matlab的关键词, 换成cload
 waves = Waves(cload, topology.Nums, 0.06);
 waves.ThreeLevel_ANPC_SingleCurrentPath(cload, 0, [1 2 3 4 5 6]);
-waves.Control(7:8, :) = waves.Control(2:3, :);
+delay = 5e-6;
+shift1 = zeros(size(waves.Control(2, :))); shift2 = shift1;
+shift1(1:end-1) = circshift(waves.Control(2, 1:end-1), round(delay/waves.Ts));
+shift1(end) = waves.Control(2, end-round(delay/waves.Ts));
+shift2(1:end-1) = circshift(waves.Control(2, 1:end-1), -round(delay/waves.Ts));
+shift2(end) = waves.Control(2, 1+round(delay/waves.Ts));
+waves.Control(7, :) = waves.Control(2, :)>0.5 & shift1>0.5 & shift2>0.5;
+shift1(1:end-1) = circshift(waves.Control(3, 1:end-1), round(delay/waves.Ts));
+shift1(end) = waves.Control(3, end-round(delay/waves.Ts));
+shift2(1:end-1) = circshift(waves.Control(3, 1:end-1), -round(delay/waves.Ts));
+shift2(end) = waves.Control(3, 1+round(delay/waves.Ts));
+waves.Control(8, :) = waves.Control(3, :)>0.5 & shift1>0.5 & shift2>0.5;
 waves.notify('ControlChanged'); % 由于是自己配置了一部分控制信号(7号和8号开关器件的信号)
                                 % 因此需要发出一个通知
 waves.ShortCircuit_Check(topology.HB_Restriction);
@@ -18,13 +29,13 @@ SiC_MOSFET = load('.\devices\Cree_C3M0015065K_SiC_650V.mat');
 Si_IGBT = load('.\devices\Infineon_IKZ75N65EL5_650V_withoutRecovery.mat');
 devices = [Si_IGBT.device SiC_MOSFET.device SiC_MOSFET.device Si_IGBT.device ...
     Si_IGBT.device Si_IGBT.device Si_IGBT.device Si_IGBT.device];
-parallel_nums = [3 3 3 3 3 3 2 2]; % 3 .* ones(1, topology.Nums); % 器件并联个数
+parallel_nums = [3 3 3 3 3 3 3 3]; % 3 .* ones(1, topology.Nums); % 器件并联个数
 Switching_Voltage = Vdc./2 .* ones(1, topology.Nums);
 losses = Losses(waves.T, waves.Ts, waves.OneCycleCurrent, waves.OneCycleControl, ...
     topology.Path, devices, parallel_nums, Switching_Voltage, topology.Device_InParallel);
 % 计算开关管上的电流
 % 由于存在器件层面的混合, 只有在明确了具体的器件以及具体的结温后才能计算开关管上的电流
-losses.JunctionTemperatureSet(175);
+losses.JunctionTemperatureSet(85);
 losses.Current_Coefficient_Calc();
 current2 = waves.Device_FlowingCurrent_Calc(2, losses.Path, losses.Device_InParallel, ...
     losses.Device_InParallel_Coefficient);
